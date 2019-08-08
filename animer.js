d64c8a0a5125f173b53423f87288fa067c6d200a
kappekoff <47227587+kappekoff@users.noreply.github.com>
2019-08-08 14:15:10 +0200

    Update animer.js

--- animer.js
+++ animer.js
@@ -2,7 +2,7 @@ var storste;
 var tilfeldigeTall = [];
 var xCanvasSize = 1500;
 var yCanvasSize = 600;
-var antallSoyler = 500;
+var antallSoyler = 50;
 var soylebredde = xCanvasSize / antallSoyler;
 var global_i = 0;
 var global_j = global_i+1;
